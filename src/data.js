0fa5db4a4b7881c9a3a2b2faf86c414bffc7a4da
Phobial <harshalpatilcsn2020@gmail.com>
2024-06-23 23:00:40 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+const blogs = [
+    {
+        id: "Reading",
+        title: "Reading",
+        content: "I read and read and read. I probably read five to six hours a day. I don’t read as fast now as when I was younger. But I read five daily newspapers. I read a fair number of magazines. I read 10-Ks. I read annual reports. I read a lot of other things, too. I’ve always enjoyed reading. I love reading biographies, for example.",
+        author: {
+            name: "Warren Buffett",
+            avtar: "https://imageio.forbes.com/specials-images/imageserve/5babb7f1a7ea4342a948b79a/0x0.jpg?format=jpg&crop=2327,2329,x748,y1753,safe&height=416&width=416&fit=bounds",
+        },
+        date: "June 22, 2024",
+        categories: ["AI Musings" , "Tech" , "TOI"],
+    },
+    {
+        id: "Starting SpaceX Musk",
+        title: "Starting SpaceX Musk",
+        content: "Elon revisited his childhood dreams of going to Mars. He visited NASA’s website one day and found no plan or even mention of going to Mars. Taken aback, he headed to Russia to see if he could buy a rocket himself. After being pushed around with ridiculous prices, Elon dove into books studying how rockets are built. On their plane ride home from Russia, he declared he would build the rocket himself with a spreadsheet detailing how to do it. This was how SpaceX was born.",
+        author: {
+            name: "Elon Musk",
+            avtar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQXu4-Scl4Wm6xzN9sYMslJ5ru9EY5gpfvHw&s",
+        },
+        date: "June 23, 2024",
+        categories: ["AI" , "Space", "Tech "],
+    },
+    
+    {
+        id: "Technology Part Of Life",
+        title: "Technology Part Of Life",
+        content: "The advance of technology based on is making it fit in so that you don't really even notice it, so it's part of everyday life..",
+        author: {
+            name: "Bill Gates",
+            avtar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIbq_PwpQReg1_JmXYvEwNL3BrgA9eMx1AAQ&s",
+        },
+        date: "June 23 2024",
+        categories: ["TOI Editorials" , "Tech" , "E-Commerce"],
+    },
+    {
+        id: "YOU JUST HAVE TO GET STARTED.",
+        title: "YOU JUST HAVE TO GET STARTED.",
+        content: "IDEAS DON'T COME OUT FULLY FORMED THEY ONLY BECOME CLEAR AS YOU WORK ON THEM.",
+        author: {
+            name: "Mark Zuckerberg",
+            avtar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaAVH1mrfwH024Fx33VkO2JQ96imtW2hrjVWPzfAUxoUrl6h05XH-ZceJ8X3uFp5S2Yxc&usqp=CAU",
+        },
+        date: "June 10, 2024",
+        categories: ["DataScienceVibes" , "Tech" , "TOI"],
+    },
+   
+    {
+        id: "No, Microsoft isn’t calling you:-007",
+        title: "No, Microsoft isn’t calling you:",
+        content: "In the digital age, as we become increasingly reliant on technology, tech support scams have emerged as a pervasive threat. These scams exploit our dependence on computers and smartphones, tricking unsuspecting users into parting with their money or personal information. Tech support scams typically involve fraudsters posing as technical support representatives from well-known companies like Microsoft, Apple, or Google. These scammers contact victims through various means, including phone calls, pop-up messages, emails, or even social media. They claim that the victim’s device is infected with a virus, running slow, or experiencing other issues that need immediate attention. Once the victim is convinced of the supposed problem, the scammer may request remote access to their device, trick them into installing malicious software, or ask for payment for unnecessary services. The end goal is to steal sensitive information, install malware, or extort money.",
+        author: {
+            name: "Rajvardhan Oak",
+            avtar: "https://i.pravatar.cc/150?img=68",
+        },
+        date: "June 3, 2024",
+        categories: ["Cyber Chronicles" , "Tech" , "TOI"],
+    },
+    {
+        id: "Don't Complain",
+        title: "Don't Complain",
+        content: "What we need to do is always lean into the future; when the world changes around you and when it changes against you what used to be a tail wind is now a head wind - you have to lean into that and figure out what to do because complaining isn't a strategy.",
+        author: {
+            name: "Jeff Bezos",
+            avtar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSK1jCtKDrvhhrz7TvQMCVJ9LStEVKJ3MLAg&s",
+        },
+        date: "Jan 5, 2023",
+        categories: ["Breaking Shackles" , "Lifestyle", "Tech"],
+    },
+    {
+        id: "AI: The key to unlocking new software companies-003",
+        title: "AI: The key to unlocking new software companies",
+        content: "The software industry is on the cusp of a revolution, thanks to Artificial Intelligence (AI). While AI’s potential is widely recognized, its impact on software business models is still largely untapped. Traditionally, software has focused on enabling existing work, but AI-enabled software can deliver outcomes, transforming the way businesses operate.The dominant SaaS business model is currently based on a per-seat model, limiting growth potential. However, AI-powered software can deliver specific business outcomes, unconstrained by employee usage. This shift unlocks new markets, enabling software to reach small businesses, under-resourced teams, and new geographies.The implications are significant. AI will expand Total Addressable Markets (TAM) in various categories, enabling niche categories to expand and large categories to grow further. New monetization variables will emerge, as businesses will pay for outcomes rather than just software.",
+        author: {
+            name: "Rajiv Dabhadkar",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/backup/rajivdprofile.jpg",
+        },
+        date: "June 17, 2024",
+        categories: ["Green Carrot" , "Business", "Tech" , "TOI"],
+    }
+]
+export default blogs
